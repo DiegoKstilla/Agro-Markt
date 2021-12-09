2d3df776c673c3233517aade0a7af01b460a0d08
DiegoKstilla <dcgomez20@gmail.com>
2021-12-09 17:02:59 -0500

    Layouts de notificaciones y publicar

--- app/src/main/java/com/example/agro_markt/view/ui/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/agro_markt/view/ui/fragments/AccountFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.agro_markt.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -12,11 +14,7 @@ import com.example.agro_markt.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class AccountFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -30,6 +28,14 @@ class AccountFragment : Fragment() {
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.btEdit)
+        button?.setOnClickListener{
+            findNavController().navigate(R.id.dashboardDetailDialog, null)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,15 +45,7 @@ class AccountFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AccountFragment().apply {
